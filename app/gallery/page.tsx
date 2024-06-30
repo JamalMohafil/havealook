564dd51f768f1bf31d53a920e09521285406f667
Jamal Mohafil <109922894+JamalMohafil@users.noreply.github.com>
2024-06-30 19:36:06 +0300

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -4,8 +4,7 @@ import React from 'react'
 type Props = {}
 
 const page = (props: Props) => {
-  return <main className=" max-w-[1920px] mx-auto ">
-    
+  return <main className=" max-w-[1920px] w-full overflow-x-hidden mx-auto ">
     <section className='mt-24'>
         <GalleryShow/>
     </section>
