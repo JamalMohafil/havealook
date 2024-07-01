937aaefecc5e96c39508499b99ec04185afc4ce2
Jamal Mohafil <109922894+JamalMohafil@users.noreply.github.com>
2024-07-01 19:11:40 +0300

    Update page.tsx

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -1,9 +1,14 @@
 import ContactUsForm from '@/components/contactUsComponents/ContactUsForm'
 import HeroContactUs from '@/components/contactUsComponents/HeroContactUs'
+import { Metadata } from 'next'
 import React from 'react'
 
 type Props = {}
+export const metadata:Metadata = {
+  title: "Have a look | تواصل معنا",
+  description: "Have a look تواصل معنا معنا من افضل انواع منتجات الاثاث التركي ",
 
+}
 const ContactUsPage = (props: Props) => {
   return (
     <main className=" max-w-[1920px] mx-auto ">
