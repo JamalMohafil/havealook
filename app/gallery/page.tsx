08f7f3bc41108043a6e3b4635f6db41c2c94ccc3
Jamal Mohafil <109922894+JamalMohafil@users.noreply.github.com>
2024-07-01 19:10:45 +0300

    Update page.tsx

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -2,8 +2,13 @@ import GalleryShow from '@/components/galleryComponents/GalleryShow';
 import React from 'react'
 
 type Props = {}
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Have a look | المعرض",
+  description: "Have a look معرضنا من افضل انواع منتجات الاثاث التركي ",
+};
 const page = (props: Props) => {
+
   return <main className=" max-w-[1920px] mx-auto ">
     
     <section className='mt-24'>
